7e381198ed1575597e6d64597bb0c57627de26f4
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-09-24 19:24:21 +0200

    Was added github action to build web-app.container on ci

--- web-app/container/src/bootstrap.tsx
+++ web-app/container/src/bootstrap.tsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 
-// create root
-window.initWebApp = (props: any): void => {
-  const root = createRoot(document.getElementById('root'));
-  root.render(
-    <App
-      user={'andrew'}
-    />);
-}
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <App
+    user={'andrew'}
+  />);
