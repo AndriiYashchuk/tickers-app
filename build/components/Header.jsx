d48cf0aab6657cfa99c75578088b13a70501fcc3
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-09-30 21:02:18 +0200

    Updates common-client package

---
+++ build/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Stack from '@mui/system/Stack';
+import { styled, useTheme } from '@mui/system';
+const Item = styled('div')(({ theme }) => ({
+    textAlign: 'center',
+    borderRadius: 4,
+}));
+const MarginLeft = styled('div')(({ theme }) => ({
+    marginLeft: '20px'
+}));
+export const Header = () => {
+    const theme = useTheme();
+    return (<AppBar position="static" elevation={0} sx={{
+            backgroundColor: theme.palette.background.default,
+            color: theme.palette.primary.main
+        }}>
+      <Toolbar>
+        <Typography>
+          Tickers
+        </Typography>
+        <MarginLeft>
+          <Stack direction="row" spacing={2}>
+            <Item>
+              <Button size={"medium"}>
+                Home
+              </Button>
+            </Item>
+            <Item>
+              <Button size={"medium"}>
+                Profile
+              </Button>
+            </Item>
+            <Item>
+              <Button size={"medium"}>
+                Reports
+              </Button>
+            </Item>
+          </Stack>
+        </MarginLeft>
+      </Toolbar>
+    </AppBar>);
+};
---
+++ build/theme/index.jsx
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#bbccee',
+        },
+        secondary: {
+            main: '#9a5252',
+        },
+        background: {
+            default: '#2e3d72',
+            paper: '#a8baec'
+        }
+    },
+    typography: {
+        fontFamily: 'Arial, sans-serif', // Шрифт по умолчанию
+    },
+});
+export default theme;
