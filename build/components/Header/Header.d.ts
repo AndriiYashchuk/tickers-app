514b594ad66ff35c3ab0158b30803ba0f296147f
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-10-10 22:51:35 +0200

    Updates common-client package

---
+++ build/components/Header/Header.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Link from '@mui/material/Link';
+interface Link {
+    title: string;
+    to?: string;
+    key?: string;
+    icon?: any;
+}
+interface Props {
+    links: Link[];
+    onClick: (selected: string) => void;
+    logo?: Link;
+    user?: {
+        name: string;
+        surname: string;
+    };
+}
+export declare const Header: ({ links, user, onClick, logo }: Props) => React.JSX.Element;
+export {};
