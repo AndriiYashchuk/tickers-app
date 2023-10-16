4d6afec98aa21621dbf376b9b3b9472b01336dce
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-10-16 20:08:33 +0200

    Updates common-client package

--- build/components/Header/constants.js
+++ build/components/Header/constants.js
@@ -1,6 +1,7 @@
 export const UNAUTHORIZED_HEADER = [
     { title: 'dashboard', to: 'web-app' },
     { title: 'about', to: 'about' },
+    { title: 'signin', to: 'signin' },
 ];
 export const LOGO = { title: 'Tickers', to: '/' };
 export const MENU = [
--- src/components/Header/constants.ts
+++ src/components/Header/constants.ts
@@ -3,6 +3,7 @@ import { Link } from '../../types/Link';
 export const UNAUTHORIZED_HEADER = [
   { title: 'dashboard', to: 'web-app' },
   { title: 'about', to: 'about' },
+  { title: 'signin', to: 'signin' },
 ];
 export const LOGO = { title: 'Tickers', to: '/' }
 
