883b92d6b0a96614dfe26a847c433c18a6543a35
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-10-17 22:30:01 +0200

    Updates common-client package

--- src/webpack/shared.js
+++ src/webpack/shared.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   'react-router-dom': {
     singleton: true,
-    requiredVersion: "6.17.0"
+    requiredVersion: "^5.2.0"
   },
   '@emotion/react': {
     singleton: true,
