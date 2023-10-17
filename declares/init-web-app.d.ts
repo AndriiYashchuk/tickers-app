ec71e01405283e938cbc067c07f27b92d7505244
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-10-17 22:08:35 +0200

    Updates common-client package

--- declares/init-web-app.d.ts
+++ declares/init-web-app.d.ts
@@ -1,7 +1,8 @@
 import { User } from '@tickers-app/common/types/User';
 
 declare global {
-  interface initWebApp {
-    initWebApp: ({ currentUser }: { currentUser: User | null }) => void
+  interface Window {
+    initWebApp: (props: { currentUser: User | null }) => void;
+    onWebAppIsReady: () => void;
   }
 }
