1c45cfad77add750f62091912a25b5e656bd05d3
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2023-11-11 22:50:20 +0100

    Testing for auth service was added

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,30 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+// import { app } from '../app';
+
+let mongo: any;
+jest.mock('../services/redis');
+jest.mock('../nats-wrapper');
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'jwt_key';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  // eslint-disable-next-line no-restricted-syntax
+  for (const collection of collections) {
+    // eslint-disable-next-line no-await-in-loop
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
