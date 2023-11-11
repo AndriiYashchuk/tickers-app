c96474becf83577cb5f46746dce655a720ee8e5a
Andrii Yashchuk <54574606+AndriiYashchuk@users.noreply.github.com>
2023-11-11 20:34:06 +0100

    Was added setuping data to Footer (#13)

--- client/src/components/layouts/main/DynamicLayout.tsx
+++ client/src/components/layouts/main/DynamicLayout.tsx
@@ -59,7 +59,14 @@ const DynamicLayout = ({
       <Main>
         {children}
       </Main>
-      <Footer />
+      <Footer
+        email="tickersapp@gmail.com"
+        author={{
+          link: 'https://www.linkedin.com/in/andrii-yashchuk',
+          name: 'Andrii Yashchuk'
+        }}
+        phone="+1234567889"
+      />
     </>
   );
 };
