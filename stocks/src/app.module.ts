7639d6f23781de777a3f53b65946398684b2e5c4
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2024-01-21 21:24:23 +0100

    Generate new nest project

---
+++ stocks/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { StocksModule } from './stocks/stocks.module';
+
+@Module({
+  imports: [UsersModule, StocksModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
