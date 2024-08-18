b602df886373013b5e5376db95bb71b917a38663
Andrii Iashchuk <iashchuk.andrii@pdffiller.team>
2024-08-18 22:26:27 +0200

    Was change google tag manager key

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -4,4 +4,4 @@ export const signinLink = '/signin';
 export const signupLink = '/signup';
 export const recaptchaPublicApiKey = '6LcXfbIoAAAAAPrSkbCMp-dunfXe33QnkcByKAIw';
 export const GA_KEY = 'G-71Y30VSHJQ';
-export const GTM_KEY = 'GT-NGWS75VQ';
+export const GTM_KEY = 'GTM-P6F65FCF';
